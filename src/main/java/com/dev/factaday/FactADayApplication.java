97f8e84007e15503b4f41c2789a5450c671b8f4e
Devadath Prabhu <dev.kts.31@gmail.com>
2021-07-16 22:34:00 +0530

    Setting up the spring project

---
+++ src/main/java/com/dev/factaday/FactADayApplication.java
@@ -0,0 +1,13 @@
+package com.dev.factaday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactADayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactADayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/factaday/FactADayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.factaday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactADayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
