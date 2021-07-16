cad39745857e48528dece5a227e92c0dec2a04ef
Devadath Prabhu <dev.kts.31@gmail.com>
2021-07-17 00:20:49 +0530

    Setting up the project

---
+++ src/main/java/com/devadath/factaday/services/Tweet.java
@@ -0,0 +1,15 @@
+package com.devadath.factaday.services;
+
+/*
+ * @author Devadath Prabhu
+ * @created Jul 16, 2021
+ */
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Tweet {
+    public void publishTweet(){
+        System.out.println(">> publishTweet()");
+    }
+}
--- src/test/java/com/devadath/factaday/FactADayApplicationTests.java
+++ src/test/java/com/devadath/factaday/FactADayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dev.factaday;
+package com.devadath.factaday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
